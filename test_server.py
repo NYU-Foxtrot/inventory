fd87fd6a5911f3b8060bec2000705105955f336d
Chrome <chromevoid500@gmail.com>
2017-10-07 18:11:50 -0400

    Add template test_server.py

---
+++ test_server.py
@@ -0,0 +1,18 @@
+# Test cases can be run with:
+# nosetests
+# coverage report -m
+
+""" Test cases for Pet Model """
+
+import unittest
+from models import Inventory, DataValidationError
+
+######################################################################
+#  T E S T   C A S E S
+######################################################################
+
+######################################################################
+#   M A I N
+######################################################################
+if __name__ == '__main__':
+    unittest.main()
