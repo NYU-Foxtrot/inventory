25a6c3469e82959447db75f2de9b8565c1d15204
Chrome <chrome@176.179.233.68.naturalwireless.com>
2017-10-07 20:51:14 -0400

    models.py: fixed serialize and deserialize functions, added find_by_quantity function; server.py: added list/get/update/delete functions; test_*.py: added test cases, coverage 91%.

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-# Copyright 2016, 2017 John Rofrano. All Rights Reserved.
+# Copyright 2017 NYU-FOXTROT. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
@@ -24,35 +24,38 @@ Attributes:
 -----------
 id (int) - uuid used to identify one inventory record 
 name (string) - the name of the product
-quantity(int) - amount of Product Inventorys
+quantity(int) - amount of Product Inventories
 status (string) - the status of the inventory : new, openBox and used
 
 """
 import threading
 import enum
 
+
 class DataValidationError(Exception):
     """ Used for an data validation errors when deserializing """
     pass
+
 
 class ProductStatus(enum.Enum):
     new = 'new'
     damaged = 'openBox'
     used = 'used'
- 
-class ProductInventory(object):
+
+
+class Inventory(object):
     """
     Class that represents 
 
-    This version uses an in-memory collection of ProductInventorys for testing
+    This version uses an in-memory collection of ProductInventories for testing
     """
     lock = threading.Lock()
     data = []
     index = 0
 
-    def __init__(self, id=0, name='', quantity=0, status=ProductStatus.new):
+    def __init__(self, inventoryid=0, name='', quantity=0, status=ProductStatus.new):
         """ Initialize a ProductInventory """
-        self.id = id
+        self.id = inventoryid
         self.name = name
         self.quantity = quantity
         self.status = status
@@ -62,21 +65,21 @@ class ProductInventory(object):
         Saves inventory (in - memory data), replace the old inventory record if it already exists
         """
         if self.id == 0:
-            self.id = self.__next_index() 
-            ProductInventory.data.append(self)
+            self.id = self.__next_index()
+            Inventory.data.append(self)
         else:
-            for i in range(len(ProductInventory.data)):
-                if ProductInventory.data[i].id == self.id:
-                    ProductInventory.data[i] = self
+            for i in range(len(Inventory.data)):
+                if Inventory.data[i].id == self.id:
+                    Inventory.data[i] = self
                     break
 
     def delete(self):
         """ Delete inventory record with specified id, name, quantiy and status"""
-        ProductInventory.data.remove(self)
+        Inventory.data.remove(self)
 
     def serialize(self):
         """ Serializes ProductInventory into a dictionary """
-        return {"id": self.id, "name": self.name, "status": self.status}
+        return {"id": self.id, "name": self.name, "quantity": self.quantity, "status": self.status}
 
     def deserialize(self, data):
         """
@@ -91,7 +94,8 @@ class ProductInventory(object):
             self.id = data['id']
         try:
             self.name = data['name']
-            self.status = data[ 'status']
+            self.quantity = data['quantity']
+            self.status = data['status']
         except KeyError as err:
             raise DataValidationError('Invalid ProductInventory: missing ' + err.args[0])
         return
@@ -99,46 +103,56 @@ class ProductInventory(object):
     @staticmethod
     def __next_index():
         """ Generates the next index in a continual sequence """
-        with ProductInventory.lock:
-            ProductInventory.index += 1
-        return ProductInventory.index
+        with Inventory.lock:
+            Inventory.index += 1
+        return Inventory.index
 
     @staticmethod
     def all():
-        """ Returns all of the ProductInventorys in the database """
-        return [productInventory for productInventory in ProductInventory.data]
+        """ Returns all of the ProductInventories in the database """
+        return [productInventory for productInventory in Inventory.data]
 
     @staticmethod
     def remove_all():
-        """ Removes all of the ProductInventorys from the database """
-        del ProductInventory.data[:]
-        ProductInventory.index = 0
-        return ProductInventory.data
+        """ Removes all of the ProductInventories from the database """
+        del Inventory.data[:]
+        Inventory.index = 0
+        return Inventory.data
 
     @staticmethod
     def find(ProductInventory_id):
         """ Finds a ProductInventory by it's ID """
-        if not ProductInventory.data:
+        if not Inventory.data:
             return None
-        ProductInventorys = [productInventory for productInventory in ProductInventory.data if productInventory.id == ProductInventory_id]
-        if ProductInventorys:
-            return ProductInventorys[0]
+        ProductInventories = [productInventory for productInventory in Inventory.data if
+                             productInventory.id == ProductInventory_id]
+        if ProductInventories:
+            return ProductInventories[0]
         return None
 
     @staticmethod
     def find_by_status(status):
-        """ Returns all of the ProductInventorys in a status
+        """ Returns all of the ProductInventories in a status
 
         Args:
-         status (string): the status of the ProductInventorys you want to match
+         status (string): the status of the ProductInventories you want to match
         """
-        return [productInventory for productInventory in ProductInventory.data if productInventory.status == status]
+        return [productInventory for productInventory in Inventory.data if productInventory.status == status]
+
+    @staticmethod
+    def find_by_quantity(quantity):
+        """ Returns all of the ProductInventories in a status
+
+        Args:
+         quantity (int): the quantity of the ProductInventories you want to match
+        """
+        return [productInventory for productInventory in Inventory.data if productInventory.quantity == quantity]
 
     @staticmethod
     def find_by_name(name):
-        """ Returns all ProductInventorys with the given name
+        """ Returns all ProductInventories with the given name
 
         Args:
-            name (string): the name of the ProductInventorys you want to match
+            name (string): the name of the ProductInventories you want to match
         """
-        return [productInventory for productInventory in ProductInventory.data if productInventory.name == name]
+        return [productInventory for productInventory in Inventory.data if productInventory.name == name]
--- test_server.py
+++ test_server.py
@@ -2,14 +2,118 @@
 # nosetests
 # coverage report -m
 
-""" Test cases for Pet Model """
+""" Test cases for the Inventory Service """
 
+import logging
 import unittest
-from models import Inventory, DataValidationError
+import json
+from mock import MagicMock, patch
+from flask_api import status  # HTTP Status Codes
+import server
+
 
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+class TestInventoryServer(unittest.TestCase):
+    """ Inventory Server Tests """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Run once before all tests """
+        server.app.debug = False
+        server.initialize_logging(logging.ERROR)
+
+    def setUp(self):
+        """ Runs before each test """
+        server.Inventory.remove_all()
+        server.Inventory(0, "shampoo", 2, "new").save()
+        server.Inventory(0, "conditioner", 5, "new").save()
+        self.app = server.app.test_client()
+
+    def tearDown(self):
+        """ Runs after each test """
+        server.Inventory.remove_all()
+
+    def test_index(self):
+        """ Test the Home Page """
+        resp = self.app.get('/')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = json.loads(resp.data)
+        self.assertEqual(data['name'], 'Inventory Demo REST API Service')
+
+    def test_get_inventory_list(self):
+        """ Get a list of Inventories """
+        resp = self.app.get('/inventories')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = json.loads(resp.data)
+        self.assertEqual(len(data), 2)
+
+    def test_get_inventory(self):
+        """ Get one Inventory """
+
+    def test_get_inventory_not_found(self):
+        """ Get a Inventory that's not found """
+
+    def test_create_inventory(self):
+        """ Create a Inventory """
+
+    def test_create_inventory_with_no_name(self):
+        """ Create a Inventory with the name missing """
+
+    def test_update_inventory(self):
+        """ Update an Inventory """
+        new_shampoo = {'name': 'shampoo', 'quantity': 8, 'status': 'new'}
+        data = json.dumps(new_shampoo)
+        resp = self.app.put('/inventories/1', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        resp = self.app.get('/inventories/1', content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        new_json = json.loads(resp.data)
+        self.assertEqual(new_json['quantity'], 8)
+
+    def test_update_inventory_with_no_name(self):
+        """ Update a Inventory with no name """
+        new_inventory = {'id': 2, 'quantity': 2, 'status': 'new'}
+        data = json.dumps(new_inventory)
+        resp = self.app.put('/inventories/2', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_inventory_not_found(self):
+        """ Update a Inventory that can't be found """
+        new_inventory = {'name': 'conditioner', 'quantity': 1, 'status': 'new'}
+        data = json.dumps(new_inventory)
+        resp = self.app.put('/inventories/0', data=data, content_type='application/json')
+        self.assertEquals(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_inventory(self):
+        """ Delete a Inventory that exists """
+        # save the current number of inventories for later comparision
+        inventory_count = self.get_inventory_count()
+        # delete a inventory
+        resp = self.app.delete('/inventories/1', content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(resp.data), 0)
+        new_count = self.get_inventory_count()
+        self.assertEqual(new_count, inventory_count - 1)
+
+    def test_query_inventory_list_by_category(self):
+        """ Query Inventories by Category """
+
+    def test_query_inventory_list_by_name(self):
+        """ Query Inventories by Name """
+
+    ######################################################################
+    # Utility functions
+    ######################################################################
+
+    def get_inventory_count(self):
+        """ save the current number of inventories """
+        resp = self.app.get('/inventories')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = json.loads(resp.data)
+        return len(data)
+
 
 ######################################################################
 #   M A I N
