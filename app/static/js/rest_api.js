2ad52bad518f2f075c13dd8f94de264c76b0de12
chromevoid <chromevoid500@gmail.com>
2017-11-18 13:36:35 -0500

    Create static folder for UI, refer: Professor's static

---
+++ app/static/js/rest_api.js
@@ -0,0 +1,227 @@
+$(function () {
+
+    // ****************************************
+    //  U T I L I T Y   F U N C T I O N S
+    // ****************************************
+
+    // Updates the form with data from the response
+    function update_form_data(res) {
+        $("#inventory_id").val(res.id);
+        $("#inventory_name").val(res.name);
+        $("#inventory_category").val(res.category);
+        if (res.available == true) {
+            $("#inventory_available").val("true");
+        } else {
+            $("#inventory_available").val("false");
+        }
+    }
+
+    /// Clears all form fields
+    function clear_form_data() {
+        $("#inventory_name").val("");
+        $("#inventory_category").val("");
+        $("#inventory_available").val("");
+    }
+
+    // Updates the flash message area
+    function flash_message(message) {
+        $("#flash_message").empty();
+        $("#flash_message").append(message);
+    }
+
+    // ****************************************
+    // Create a inventory
+    // ****************************************
+
+    $("#create-btn").click(function () {
+
+        var name = $("#inventory_name").val();
+        var category = $("#inventory_category").val();
+        var available = $("#inventory_available").val() == "true";
+
+        var data = {
+            "name": name,
+            "category": category,
+            "available": available
+        };
+
+        var ajax = $.ajax({
+            type: "POST",
+            url: "/inventories",
+            contentType:"application/json",
+            data: JSON.stringify(data),
+        });
+
+        ajax.done(function(res){
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+    });
+
+
+    // ****************************************
+    // Update a inventory
+    // ****************************************
+
+    $("#update-btn").click(function () {
+
+        var inventory_id = $("#inventory_id").val();
+        var name = $("#inventory_name").val();
+        var category = $("#inventory_category").val();
+        var available = $("#inventory_available").val() == "true";
+
+        var data = {
+            "name": name,
+            "category": category,
+            "available": available
+        };
+
+        var ajax = $.ajax({
+                type: "PUT",
+                url: "/inventories/" + inventory_id,
+                contentType:"application/json",
+                data: JSON.stringify(data)
+            })
+
+        ajax.done(function(res){
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Retrieve a inventory
+    // ****************************************
+
+    $("#retrieve-btn").click(function () {
+
+        var inventory_id = $("#inventory_id").val();
+
+        var ajax = $.ajax({
+            type: "GET",
+            url: "/inventories/" + inventory_id,
+            contentType:"application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            clear_form_data()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Delete a inventory
+    // ****************************************
+
+    $("#delete-btn").click(function () {
+
+        var inventory_id = $("#inventory_id").val();
+
+        var ajax = $.ajax({
+            type: "DELETE",
+            url: "/inventories/" + inventory_id,
+            contentType:"application/json",
+            data: '',
+        })
+
+        ajax.done(function(res){
+            clear_form_data()
+            flash_message("inventory with ID [" + res.id + "] has been Deleted!")
+        });
+
+        ajax.fail(function(res){
+            flash_message("Server error!")
+        });
+    });
+
+    // ****************************************
+    // Clear the form
+    // ****************************************
+
+    $("#clear-btn").click(function () {
+        $("#inventory_id").val("");
+        clear_form_data()
+    });
+
+    // ****************************************
+    // Search for a inventory
+    // ****************************************
+
+    $("#search-btn").click(function () {
+
+        var name = $("#inventory_name").val();
+        var category = $("#inventory_category").val();
+        var available = $("#inventory_available").val() == "true";
+
+        var queryString = ""
+
+        if (name) {
+            queryString += 'name=' + name
+        }
+        if (category) {
+            if (queryString.length > 0) {
+                queryString += '&category=' + category
+            } else {
+                queryString += 'category=' + category
+            }
+        }
+        if (available) {
+            if (queryString.length > 0) {
+                queryString += '&available=' + available
+            } else {
+                queryString += 'available=' + available
+            }
+        }
+
+        var ajax = $.ajax({
+            type: "GET",
+            url: "/inventories?" + queryString,
+            contentType:"application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            $("#search_results").empty();
+            $("#search_results").append('<table class="table-striped">');
+            var header = '<tr>'
+            header += '<th style="width:10%">ID</th>'
+            header += '<th style="width:40%">Name</th>'
+            header += '<th style="width:40%">Category</th>'
+            header += '<th style="width:10%">Available</th></tr>'
+            $("#search_results").append(header);
+            for(var i = 0; i < res.length; i++) {
+                inventory = res[i];
+                var row = "<tr><td>"+inventory.id+"</td><td>"+inventory.name+"</td><td>"+inventory.category+"</td><td>"+inventory.available+"</td></tr>";
+                $("#search_results").append(row);
+            }
+
+            $("#search_results").append('</table>');
+
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+})
